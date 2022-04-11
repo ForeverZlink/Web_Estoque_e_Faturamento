01ba4bd02539925ae0d535132812a82208346356
Carlos Henrique <carloscunha080@gmail.com>
2022-04-10 21:46:59 -0300

    criei o template de novo produto e adicionei um botão de criar produto no html live server

--- Web_Estoque_E_Faturamento/Controllers/Product.cs
+++ Web_Estoque_E_Faturamento/Controllers/Product.cs
@@ -2,12 +2,12 @@ using Microsoft.AspNetCore.Mvc;
 using System.Text.Encodings.Web;
 
 
-namespace Web_Estoque_E_Faturamento.Controllers
+namespace Web_Estoque_E_Faturamento.ControllersProduct
 {
     public class ProductController : Controller
     {
         
-        public IActionResult Index()
+        public  IActionResult Index()
         {
             ViewData["Message"]= Environment.GetEnvironmentVariable("Anime");
             return View();
@@ -15,6 +15,9 @@ namespace Web_Estoque_E_Faturamento.Controllers
 
         // 
         // GET: /OrderService/Welcome/ 
+        public IActionResult NewProduct(){
+            return View();
+        }
 
         
 
