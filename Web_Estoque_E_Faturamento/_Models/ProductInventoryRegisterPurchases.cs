c4fcca20ad1214045188e9774a2d17ec643e9067
Carlos Henrique Cunha de Oliveira <carloscunha080@gmail.com.br>
2022-06-25 21:49:30 -0300

    Fiz uma pequena alteração e adicionei um marcado de foreign key para o model product

--- Web_Estoque_E_Faturamento/_Models/ProductInventoryRegisterPurchases.cs
+++ Web_Estoque_E_Faturamento/_Models/ProductInventoryRegisterPurchases.cs
@@ -4,17 +4,22 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 namespace Web_Estoque_E_Faturamento._Models
 {
     public class ProductInventoryRegisterPurchase
     {
         [Key]
         public int Id {get;set;}
-        public Product Product {get;set;}
         public float QuantityBuyed {get;set;}
         public string Provider{get;set;}
         public string DateOfPurchase {get;set;}
         public float PriceOfPurchase {get;set;}
         public float PriceProductUnity {get;set;}
+        
+        [ForeignKey("Product")]
+        public int ProductId{get;set;}
+        public Product Product {get;set;}
+       
     }
 }
