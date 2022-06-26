0c93f4f68bac1e551d810c3d3c3d23ba7c06c07b
Carlos Henrique Cunha de Oliveira <carloscunha080@gmail.com>
2022-06-26 20:44:02 -0300

    importei os modulos de componentes de models

--- Web_Estoque_E_Faturamento/_Models/Provider.cs
+++ Web_Estoque_E_Faturamento/_Models/Provider.cs
@@ -2,11 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 namespace Web_Estoque_E_Faturamento._Models
 {
     public class Provider
     {
+        [Key]
         public int Id {get;set;}
         public string Name {get;set;}
         public string Andress {get;set;}
