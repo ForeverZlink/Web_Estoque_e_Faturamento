442764b42e4ae0907143796ee4e91324ba34b5db
Carlos Henrique Cunha de Oliveira <carloscunha080@gmail.com>
2022-07-03 21:31:27 -0300

    Deixei o construtor mais organizado e légivel

--- Web_Estoque_E_Faturamento/ViewModels/DashBoardContextNecessary.cs
+++ Web_Estoque_E_Faturamento/ViewModels/DashBoardContextNecessary.cs
@@ -8,7 +8,10 @@ namespace Web_Estoque_E_Faturamento.ViewModels
 {
     public class DashBoardContextNecessary
     {
-        public DashBoardContextNecessary ( IEnumerable<Provider>  provider,IEnumerable<Product> product, IEnumerable<ProductInventoryRegisterPurchase> productInventoryRegisterPurchase){
+        public DashBoardContextNecessary ( 
+            IEnumerable<Provider>  provider,IEnumerable<Product> product, 
+            IEnumerable<ProductInventoryRegisterPurchase> productInventoryRegisterPurchase)
+        {
             this.Product=product;
             this.Provider=provider;
             this.ProductInventoryRegisterPurchase=productInventoryRegisterPurchase;         
