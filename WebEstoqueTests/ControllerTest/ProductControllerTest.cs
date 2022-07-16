b07920ae5b105d9aba51f34b1c23116c43c94a95
Carlos Henrique Cunha de Oliveira <carloscunha080@gmail.com>
2022-07-16 20:33:07 -0300

    Recrivi o test Details, agora o contexto é gerado pelo o mock

--- WebEstoqueTests/ControllerTest/ProductControllerTest.cs
+++ WebEstoqueTests/ControllerTest/ProductControllerTest.cs
@@ -5,21 +5,44 @@ using Web_Estoque_E_Faturamento._Models;
 using WebEstoqueTests;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 
+using System.Linq;
+
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
 using System;
 
 namespace WebEstoqueTests
 {
-    public class ProductControllerTest:Controller
+    
+    public interface IProductRepository{
+        public void Edit();
+
+        public Product Details(int id);
+    }
+    
+    
+    
+    public class ProductControllerTest:IClassFixture<TestDatabaseFixture>
     {
+       
+       
+       static public TestDatabaseFixture? Fixture;
         public string ViewResult = "ViewResult";
         public string NotFoundResult = "NotFoundResult";
         public string RedirectToActionResult = "RedirectToActionResult";
-        static DbContextOptions<MvcProductContext> option = new DbContextOptionsBuilder<MvcProductContext>().UseInMemoryDatabase(databaseName:"ProductContextDatabase").Options;
-        MvcProductContext context = new MvcProductContext(option);
+        
+        
+        MvcProductContext context = Fixture.CreateContext();
+       
+        
+        
         Product ProductModelData = new Product(){
             Name="Car",Code="01",Description="A nice car"
            };
+        
+        
         public ProductController _ProductController;
         public dynamic ContextConfig{
             get{return context;}
@@ -36,6 +59,7 @@ namespace WebEstoqueTests
         [Fact]
         public void Edit()
         {
+            
             //This part will be test Edit method when acess via get, thus
             //its just search in database for the informations
 
@@ -71,24 +95,92 @@ namespace WebEstoqueTests
             };
             var ResponseIdEqualButNotExistAProductWithId =ProductControllerInstance.Edit(id:ProductModelDataButWithoutSaveInDatabase.Id, product:ProductModelDataButWithoutSaveInDatabase);
             Assert.Contains(this.NotFoundResult, ResponseIdEqualButNotExistAProductWithId.Result.ToString());
-
+            this.ContextConfig.Dispose();
 
         }
         [Fact]
         public void Details()
         {
-            // Case when its a Product  te jwenn
-             ContextConfig=ProductModelData;
-             ProductControllerInstance = ContextConfig;
-
-             var ResponseWanted  = ProductControllerInstance.Details(id:1);
-           
+            var productData =  new Product{Id=44,Name="Car",Code="01",Description="A nice car"};
              
+
+            ProductInventory ProductInventory = new ProductInventory(){
+                ProductId=productData.Id,Product=productData,
+                QuantityInStock=0,ProductInventoryRegisterPurchase= new List<ProductInventoryRegisterPurchase>()};
+
+           
+
+            ProductInventoryRegisterPurchase ProductInvetoryRegisterPurchaseModelData = new ProductInventoryRegisterPurchase(){
+            QuantityBuyed=1,PriceOfPurchase=20,DateOfPurchase=DateTime.Today.ToString(),
+            PriceProductUnity=2,ProductId=productData.Id,
+            Product=productData
+                };
+            ProductInventory.ProductInventoryRegisterPurchase.Add(ProductInvetoryRegisterPurchaseModelData);
+            
+            var data = new List<Product>{
+                productData
+               
+            }.AsQueryable();
+
+            var dataProductInventoryPurchase = new List<ProductInventoryRegisterPurchase>{
+                ProductInvetoryRegisterPurchaseModelData
+            }.AsQueryable();
+            
+                
+            var dataProductInventory = new List<ProductInventory>{
+                ProductInventory
+            }.AsQueryable();
+
+            
+            
+            var mockset = new Mock<DbSet<Product>>();
+            mockset.As<IQueryable<Product>>().Setup(m=>m.Provider);
+            mockset.As<IQueryable<Product>>().Setup(m => m.Expression).Returns(data.Expression);
+            mockset.As<IQueryable<Product>>().Setup(m => m.ElementType).Returns(data.ElementType);
+            mockset.As<IQueryable<Product>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+
+            var mocksetInventory = new Mock<DbSet<ProductInventory>>();
+            mockset.As<IQueryable<ProductInventory>>().Setup(m=>m.Provider);
+            mockset.As<IQueryable<ProductInventory>>().Setup(m => m.Expression).Returns(dataProductInventory.Expression);
+            mockset.As<IQueryable<ProductInventory>>().Setup(m => m.ElementType).Returns(dataProductInventory.ElementType);
+            mockset.As<IQueryable<ProductInventory>>().Setup(m => m.GetEnumerator()).Returns(dataProductInventory.GetEnumerator());
+
+            var mocksetPurchases = new Mock<DbSet<ProductInventoryRegisterPurchase>>();
+            mockset.As<IQueryable<ProductInventoryRegisterPurchase>>().Setup(m=>m.Provider);
+            mockset.As<IQueryable<ProductInventoryRegisterPurchase>>().Setup(m => m.Expression).Returns(dataProductInventoryPurchase.Expression);
+            mockset.As<IQueryable<ProductInventoryRegisterPurchase>>().Setup(m => m.ElementType).Returns(dataProductInventoryPurchase.ElementType);
+            mockset.As<IQueryable<ProductInventoryRegisterPurchase>>().Setup(m => m.GetEnumerator()).Returns(dataProductInventoryPurchase.GetEnumerator());
+
+            var mockContext  = new Mock<MvcProductContext>();
+            mockContext.Setup(x=>x.Product).Returns(mockset.Object);
+            mockContext.Setup(x=>x.ProductInventory).Returns(mocksetInventory.Object);
+            mockContext.Setup(x=>x.ProductInventoryRegisterPurchase).Returns(mocksetPurchases.Object);
+            
+
+           
+            
+            var logger = LoggerFactory.Create(config=>{config.AddConsole();}).CreateLogger("Program");
+
+         
+            
+            
+            ProductControllerInstance = mockContext.Object;
+           
+
+            // Case when its a Product  te jwenn
+            
+
+             
+
+             var ResponseWanted  = ProductControllerInstance.Details(id:productData.Id);
+           
+             logger.LogInformation($"Hello {ResponseWanted.Result.ToString()}");
 
              Assert.Contains(this.ViewResult, ResponseWanted.Result.ToString());
 
              //Case when doesn't have a product with this id
              var ResponseWantedIdNotExists = ProductControllerInstance.Details(id:9999);
+            
              Assert.Contains(this.NotFoundResult,ResponseWantedIdNotExists.Result.ToString());
 
 
@@ -102,12 +194,14 @@ namespace WebEstoqueTests
         [Fact]
         public async void Delete()
         {
-            Product ProductModelData = new Product(){
+            MvcProductContext context = Fixture.CreateContext();
+            this.ProductControllerInstance=context;
+            Product ProductModelData = new Product(){Id=3222,
             Name="Car",Code="01",Description="A nice car"
            };
            ContextConfig=ProductModelData;
            ProductControllerInstance = ContextConfig;
-           var ResponseWanted  = ProductControllerInstance.DeleteConfirmed(id:1);
+           var ResponseWanted  = ProductControllerInstance.DeleteConfirmed(id:this.ProductModelData.Id);
            context.Dispose();
            Console.WriteLine(ResponseWanted.Result.ToString());
            Assert.Contains("RedirectToActionResult",ResponseWanted.Result.ToString());
@@ -115,6 +209,8 @@ namespace WebEstoqueTests
         }
         [Fact]
         public async void CreateOn(){
+            MvcProductContext context = Fixture.CreateContext();
+            this.ProductControllerInstance=context;
             Product ProductModelData = new Product(){
             Name="Car2",Code="02",Description="A nice car"
            };
@@ -131,6 +227,7 @@ namespace WebEstoqueTests
            }catch(Exception e ){
              
            }
+           context.Dispose();
            
 
         }
