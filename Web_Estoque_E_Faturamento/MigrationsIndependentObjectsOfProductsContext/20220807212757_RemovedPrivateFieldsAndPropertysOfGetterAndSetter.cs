c4d1e319c1086003e50c48d65d0f3c60b846bbbf
Carlos Henrique <carloscunha080@gmail.com>
2022-08-07 18:59:12 -0300

    Removi os campos privados e seus valores pré definidos. Esse campos estavam gerando erros de código assincrono. Também removi as propriedades DateOfCreation, AlreadyBuyed
    O campo AlreadyBuyed teve seu underline removido.
    
    Todas as migrations foram criadas e aplicadas no banco de dados da produção.

---
+++ Web_Estoque_E_Faturamento/MigrationsIndependentObjectsOfProductsContext/20220807212757_RemovedPrivateFieldsAndPropertysOfGetterAndSetter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Web_Estoque_E_Faturamento.MigrationsIndependentObjectsOfProductsContext
+{
+    public partial class RemovedPrivateFieldsAndPropertysOfGetterAndSetter : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AlreadyBuyed",
+                table: "ProductListReminderToBuyWithoutUseProductAlreadyRegistered",
+                newName: "_AlreadyBuyed");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "_AlreadyBuyed",
+                table: "ProductListReminderToBuyWithoutUseProductAlreadyRegistered",
+                newName: "AlreadyBuyed");
+        }
+    }
+}
--- Web_Estoque_E_Faturamento/_Models/ProductListReminderToBuyWithoutUseProductAlreadyRegistered.cs
+++ Web_Estoque_E_Faturamento/_Models/ProductListReminderToBuyWithoutUseProductAlreadyRegistered.cs
@@ -10,32 +10,19 @@ namespace Web_Estoque_E_Faturamento._Models
 {
     public class ProductListReminderToBuyWithoutUseProductAlreadyRegistered
     {
+      
+        
         [Key]
         public int Id {get;set;}
         
-        private string DateOfCreationDefault = DateTime.Now.ToString();
+        public string DateOfCreation { get;set;}
         public string NameOfProduct{get;set;}
         public string CodeOfProduct{get;set;}
-        public string DateOfCreation {
-            get{
-                return DateOfCreationDefault;
-            }
-            set{
-                ;
-            }
-        }
+       
 
-        private bool _AlreadyBuyed = false;
+        public  bool AlreadyBuyed { get; set; }
 
-        public bool AlreadyBuyed {
-            get{
-                return AlreadyBuyed;
-            }
-            set{
-                _AlreadyBuyed= value;
-            }
-        }
-
+      
         
        
     }
