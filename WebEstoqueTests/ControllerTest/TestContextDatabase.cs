3b8e07ac5e89662f68953d523c4ea1a726f6b0a6
Carlos Henrique <carloscunha080@gmail.com>
2022-08-06 22:06:11 -0300

    Adicionei uma nova Connection String, para que possa existir outro banco de dados na classe de geração de contexto para testes de database. Além disso, criei um método que retorna um contexto usando o banco de dados citado na connection string. É necessário ter esse banco de dados com esse contexto para teste porque o controller ProductListReminderToBuyWithoutUseProductAlreadyRegistered recebe um contexto que é ligado a uma database diferente.

--- WebEstoqueTests/ControllerTest/TestContextDatabase.cs
+++ WebEstoqueTests/ControllerTest/TestContextDatabase.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 public class TestDatabaseFixture
 {
     private const string ConnectionString = @"Server=(localdb)\mssqllocaldb;Database=EFTestSample;Trusted_Connection=True";
-
+    private const string ConnectionStringMvcIndependentObjectsOfProductsContext = @"Server=(localdb)\mssqllocaldb;Database=MvcIndependentObjectsOfProductsContext;Trusted_Connection=True";
     private static readonly object _lock = new();
     private static bool _databaseInitialized;
 
@@ -13,7 +13,7 @@ public class TestDatabaseFixture
         {
             if (!_databaseInitialized)
             {
-                using (var context = CreateContext())
+                 using (var context = CreateContext())
                 {
                     context.Database.EnsureDeleted();
                     context.Database.EnsureCreated();
@@ -33,4 +33,13 @@ public class TestDatabaseFixture
                 .Options);
 
     }
+    public MvcIndependentObjectsOfProductsContext CreateMvcIndependentContext(){
+        return new MvcIndependentObjectsOfProductsContext(
+            new DbContextOptionsBuilder<MvcIndependentObjectsOfProductsContext>()
+            .UseSqlServer(ConnectionStringMvcIndependentObjectsOfProductsContext)
+            .Options        
+            );
+    }
+
+
 }
