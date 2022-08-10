c878a1302a6e8cf019b8aa1ce5d96d75b93fcfac
Carlos Henrique <carloscunha080@gmail.com>
2022-08-09 21:36:59 -0300

    Adicionei algums campos  a classe e um construtor para ser possível injetar dependências a esses campos;
    1-O primeiro campo é para o diretório de criação do arquivo
    
    2-Campo é para os valores que estarão no título da planilha
    
    3-Os valores que serão inseridos na tabela.

--- Web_Estoque_E_Faturamento/ClassUtilities/ExcelHandler.cs
+++ Web_Estoque_E_Faturamento/ClassUtilities/ExcelHandler.cs
@@ -1,6 +1,21 @@
-﻿namespace Web_Estoque_E_Faturamento.ClassUtilities
+﻿using OfficeOpenXml;
+namespace Web_Estoque_E_Faturamento.ClassUtilities
 {
     public class ExcelHandler
     {
+        public string DirectoryToExcelCreation;
+        public string[] ValuesToColumsTitles;
+        public string[] ValuesToRows;
+        public ExcelHandler(
+            string directoryToExcelCreation, 
+            string[]valuesToColumsTitles,
+            string[]valuesToRow)
+        {
+            DirectoryToExcelCreation = directoryToExcelCreation;
+            ValuesToColumsTitles = valuesToColumsTitles;
+            ValuesToRows = valuesToRow;
+
+        }
+
     }
 }
